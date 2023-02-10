9ecd4ca3a16938290f98e219ebefe3cf67db6de0
prostonau <prostonau@mail.ru>
2023-02-11 02:01:27 +0300

    init | towel sort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,16 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+   
+  let b = []
+  if (matrix == [] || !matrix ) return [];
+  if ( matrix.length == 0 ) return [];
+  
+  b = matrix.map( (e,i) => {
+    if(i%2 == 0) return e
+    else return e.reverse()
+  })
+  
+  
+  return b.reduce ((a,c) => a.concat(c))
 }
